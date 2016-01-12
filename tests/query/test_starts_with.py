f9d46444ee43c1aef29f183b3afaff0256a5e528
adria <adf7595228>
2016-01-12 22:41:47 +0100

    Adding the following case sensitive String operators: - contains - endswith - exact - startswith
    No idea how to include the /i flag to execute no case sensitive queries and create icontains, iendswith, iexact and istartswith operators. Any help will be appreciated!

--- tests/query/test_starts_with.py
+++ tests/query/test_starts_with.py
@@ -12,7 +12,7 @@ class TestStartsWithOperator(AsyncTestCase):
     def test_to_query(self):
         query = StartsWithOperator()
         expect(query).not_to_be_null()
-        expect(query.to_query("field_name", "^Ber")).to_be_like({
+        expect(query.to_query("field_name", "Ber")).to_be_like({
             "field_name": {
                 "$regex": "^Ber"
             }
