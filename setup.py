551b2b878653edc03e6476856fa4337c54eee7e1
Sergey Punkoff <Sergey Punkoff>
2016-06-08 11:25:50 +0300

    fix versions of requires

--- setup.py
+++ setup.py
@@ -47,9 +47,9 @@ MotorEngine is a port of the amazing MongoEngine Mapper. Instead of using pymong
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'pymongo==2.7',
+        'pymongo>2.7',
         'tornado',
-        'motor==0.2',
+        'motor>=0.6',
         'six',
         'easydict'
     ],
