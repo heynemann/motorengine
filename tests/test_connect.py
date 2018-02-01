30153c40366833fc03ad2b277226674b3eacad24
Daniel Kiedrowski <daniel@levire.com>
2018-02-01 21:52:45 +1300

    The replicaSet parameter is optional in newer versions of PyMongo, it will defer the name of the replica set by the URIs if possible.

--- tests/test_connect.py
+++ tests/test_connect.py
@@ -27,12 +27,3 @@ class TestConnect(AsyncTestCase):
         args, kwargs = self.exec_async(db.ping)
         ping_result = args[0]['ok']
         expect(ping_result).to_equal(1.0)
-
-    def test_connect_to_replica_set_with_invalid_replicaset_name(self):
-        try:
-            connect('test', host="localhost:27017,localhost:27018", replicaSet=0, port=27017, io_loop=self.io_loop)
-        except ConnectionError:
-            exc_info = sys.exc_info()[1]
-            expect(str(exc_info)).to_include('the replicaSet keyword parameter is required')
-        else:
-            assert False, "Should not have gotten this far"
